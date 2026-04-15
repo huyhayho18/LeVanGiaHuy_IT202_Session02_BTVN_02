50fdb02ef73202a03a8b9bd18c1796b1f6869771
huyhayho18 <giahuylevan1807@gmail.com>
2026-04-15 21:20:19 +0700

    Add files via upload

--- bai2.sql
+++ bai2.sql
@@ -1,6 +1,12 @@
 create table customers (
 	customersId int primary key,
     fullName varchar(100) not null,
-    email varchar(100) not null unique,
-    age int check (age > 0) not null
-)
+    email varchar(100),
+    age int
+);
+
+alter table customers
+change email email varchar(100) not null;
+
+alter table customers
+change age age varchar(100) not null;
